a215575d55b95f6202e466d597479adb8065473f
francisco rodriguez <francisco.rodriguez@lanek.cl>
2023-03-08 02:43:30 +0000

    Update data

--- Inventario.py
+++ Inventario.py
@@ -1,5 +1,4 @@
 import os
-import schedule
 import gspread
 import Requests as rq
 import threading
@@ -22,9 +21,8 @@ Ent_sheet = Entry_sheet.worksheet('Entradas')
 Out_sheet = Outs_sheet.worksheet('Salidas')
 Inv_sheet = Stock_sheet.worksheet('Inventario')
 
-
-last_edited_entry   = datetime.strptime(Ent_sheet.col_values(2)[-1], '%d/%m/%Y %H:%M:%S').time()
-last_edited_out     = datetime.strptime(Out_sheet.col_values(1)[-1], '%d/%m/%Y %H:%M:%S').time()
+last_edited_entry = datetime.strptime(Ent_sheet.col_values(2)[-1], '%d/%m/%Y %H:%M:%S').time()
+last_edited_out = datetime.strptime(Out_sheet.col_values(1)[-1], '%d/%m/%Y %H:%M:%S').time()
 
 if not os.path.exists("log.txt"):
     with open("log.txt", "w") as file:
@@ -38,44 +36,44 @@ if not os.path.exists("outs.txt"):
     with open("outs.txt", "w") as file:
         pass
 
-def inventario():
-    try:
-        #total_rows = len(Ent_sheet.col_values(2)) - 1   #only data
+def main(last_edited_entry, last_edited_out):
+    #schedule.every(10).seconds.do(inventario)
 
-        edited_entry    = datetime.strptime(Ent_sheet.col_values(2)[-1], '%d/%m/%Y %H:%M:%S').time()
-        edited_out      = datetime.strptime(Out_sheet.col_values(1)[-1], '%d/%m/%Y %H:%M:%S').time()
+    while True:
+        try:
+            #total_rows = len(Ent_sheet.col_values(2)) - 1   #only data
 
-        if edited_entry > last_edited_entry : 
-            Entry_t = threading.Thread(target=rq.New_entry, args=(Ent_sheet, Inv_sheet))
-            #rq.New_entry(Ent_sheet, Inv_sheet)
-            Entry_t.start()
-            last_edited_entry = edited_entry
-            print("Entry updated at "+str(last_edited_entry))
+            edited_entry    = datetime.strptime(Ent_sheet.col_values(2)[-1], '%d/%m/%Y %H:%M:%S').time()
+            edited_out      = datetime.strptime(Out_sheet.col_values(1)[-1], '%d/%m/%Y %H:%M:%S').time()
 
+            if edited_entry > last_edited_entry : 
+                Entry_t = threading.Thread(target=rq.New_entry, args=(Ent_sheet, Inv_sheet))
+                #rq.New_entry(Ent_sheet, Inv_sheet)
+                Entry_t.start()
+                last_edited_entry = edited_entry
+                print("Entry updated at "+str(last_edited_entry))
+
+                with open("log.txt", "a") as file:
+                    file.write("Entry updated at "+str(last_edited_entry) + "\n")
+
+            elif edited_out > last_edited_out : 
+                Out_t = threading.Thread(target=rq.New_out, args=(Out_sheet, Inv_sheet))
+                #rq.New_out(Out_sheet, Inv_sheet)
+                Out_t.start()
+                last_edited_out = edited_out
+                print("Out updated at "+str(last_edited_out))
+
+                with open("log.txt", "a") as file:
+                    file.write("Out updated at "+str(last_edited_entry) + "\n")
+
+        except Exception as e:
+            # Print the error message
+            print("An error occurred:", str(e))
             with open("log.txt", "a") as file:
-                file.write("Entry updated at "+str(last_edited_entry) + "\n")
-
-        elif edited_out > last_edited_out : 
-            Out_t = threading.Thread(target=rq.New_out, args=(Out_sheet, Inv_sheet))
-            #rq.New_out(Out_sheet, Inv_sheet)
-            Out_t.start()
-            last_edited_out = edited_out
-            print("Out updated at "+str(last_edited_out))
-
-            with open("log.txt", "a") as file:
-                file.write("Out updated at "+str(last_edited_entry) + "\n")
-            
+                file.write("Error : \n" + str(e) + "\n")
         print("Waiting")
-
-    except Exception as e:
-        # Print the error message
-        print("An error occurred:", str(e))
-        with open("log.txt", "a") as file:
-            file.write("Error : \n" + str(e) + "\n")
-
-def main():
-    schedule.every(10).seconds.do(inventario)
+        sleep(10)
 
 
 if __name__ == "__main__":
-    main()
+    main(last_edited_entry, last_edited_out)
