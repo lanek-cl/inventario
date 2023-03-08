a49c76b4159d94cc9caf698847cce8536738faa2
francisco rodriguez <francisco.rodriguez@lanek.cl>
2023-03-08 03:43:33 +0000

    last updated time to file and cron restart job

--- Inventario.py
+++ Inventario.py
@@ -21,12 +21,17 @@ Ent_sheet = Entry_sheet.worksheet('Entradas')
 Out_sheet = Outs_sheet.worksheet('Salidas')
 Inv_sheet = Stock_sheet.worksheet('Inventario')
 
-last_edited_entry = datetime.strptime(Ent_sheet.col_values(2)[-1], '%d/%m/%Y %H:%M:%S').time()
-last_edited_out = datetime.strptime(Out_sheet.col_values(1)[-1], '%d/%m/%Y %H:%M:%S').time()
-
 if not os.path.exists("log.txt"):
     with open("log.txt", "w") as file:
         pass
+
+if not os.path.exists("last_updates.txt"):
+    with open("last_updates.txt", "w") as file:     #format:  entry, out
+        init_last_entry = datetime.strptime(Ent_sheet.col_values(2)[-1], "%d/%m/%Y %H:%M:%S")
+        init_last_out = datetime.strptime(Out_sheet.col_values(1)[-1], "%d/%m/%Y %H:%M:%S")
+        #print(init_last_entry)
+        file.write(init_last_entry.strftime("%d/%m/%Y %H:%M:%S") + "," + init_last_out.strftime("%d/%m/%Y %H:%M:%S"))
+
 
 if not os.path.exists("entries.txt"):
     with open("entries.txt", "w") as file:
@@ -36,6 +41,13 @@ if not os.path.exists("outs.txt"):
     with open("outs.txt", "w") as file:
         pass
 
+with open("last_updates.txt", "r") as file:
+    line = file.read().split(',')
+    last_edited_entry = datetime.strptime(line[0], "%d/%m/%Y %H:%M:%S")
+    last_edited_out = datetime.strptime(line[1], "%d/%m/%Y %H:%M:%S")
+    #print(last_edited_entry)
+
+
 def main(last_edited_entry, last_edited_out):
     #schedule.every(10).seconds.do(inventario)
 
@@ -43,8 +55,8 @@ def main(last_edited_entry, last_edited_out):
         try:
             #total_rows = len(Ent_sheet.col_values(2)) - 1   #only data
 
-            edited_entry    = datetime.strptime(Ent_sheet.col_values(2)[-1], '%d/%m/%Y %H:%M:%S').time()
-            edited_out      = datetime.strptime(Out_sheet.col_values(1)[-1], '%d/%m/%Y %H:%M:%S').time()
+            edited_entry    = datetime.strptime(Ent_sheet.col_values(2)[-1], '%d/%m/%Y %H:%M:%S')
+            edited_out      = datetime.strptime(Out_sheet.col_values(1)[-1], '%d/%m/%Y %H:%M:%S')
 
             if edited_entry > last_edited_entry : 
                 Entry_t = threading.Thread(target=rq.New_entry, args=(Ent_sheet, Inv_sheet))
@@ -55,6 +67,9 @@ def main(last_edited_entry, last_edited_out):
 
                 with open("log.txt", "a") as file:
                     file.write("Entry updated at "+str(last_edited_entry) + "\n")
+                
+                with open("last_updates.txt", "w") as update_e:
+                    update_e.write(edited_entry.strftime("%d/%m/%Y %H:%M:%S") + "," + last_edited_out.strftime("%d/%m/%Y %H:%M:%S"))
 
             elif edited_out > last_edited_out : 
                 Out_t = threading.Thread(target=rq.New_out, args=(Out_sheet, Inv_sheet))
@@ -65,6 +80,9 @@ def main(last_edited_entry, last_edited_out):
 
                 with open("log.txt", "a") as file:
                     file.write("Out updated at "+str(last_edited_entry) + "\n")
+
+                with open("last_updates.txt", "w") as update_e:
+                    update_e.write(last_edited_entry.strftime("%d/%m/%Y %H:%M:%S") + "," + edited_out.strftime("%d/%m/%Y %H:%M:%S"))
 
         except Exception as e:
             # Print the error message
